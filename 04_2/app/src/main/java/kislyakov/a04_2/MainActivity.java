d1e5324524ef26e89fb4ce19553f9ce4d2420d97
Aleksey <alekskis95@gmail.com>
2019-02-10 16:15:35 +0300

    Lesson 04 ready
    Delete 04_1
    04_2 ended

--- 04_2/app/src/main/java/kislyakov/a04_2/MainActivity.java
+++ 04_2/app/src/main/java/kislyakov/a04_2/MainActivity.java
@@ -1,10 +1,18 @@
 package kislyakov.a04_2;
 
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,19 +20,89 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView numbersList;
     private NumbersAdapter numbersAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         numbersList = findViewById(R.id.rv_data);
+        numbersList.setHasFixedSize(true);
         GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+
         numbersList.setLayoutManager(layoutManager);
 
-        numbersAdapter = new NumbersAdapter(5);
+
+        numbersAdapter = new NumbersAdapter(10, new NumbersAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(String item, View v) {
+                Snackbar.make(v,item,Toast.LENGTH_SHORT).setAction("Action", null).show();;
+
+            }
+        });
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                switch(numbersAdapter.getItemViewType(position)){
+                    case NumbersAdapter.DESCRIPTION_RED:
+                        return 1;
+                    case NumbersAdapter.DEFAULT:
+                        return 1;
+                    case NumbersAdapter.ONEINTWO:
+                        return 2;
+                    case NumbersAdapter.BASECELL:
+                        return 2;
+                    default:
+                        return -1;
+                }
+            }
+        });
         numbersList.setAdapter(numbersAdapter);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.ic_info_white) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Information");
+            builder.setMessage("Hey brother, you've clicked the info");
+
+            // add a button
+            builder.setPositiveButton("OK", null);
+
+            // create and show the alert dialog
+            AlertDialog dialog = builder.create();
+            dialog.show();;
+            return true;
+        }
+        else if (id == R.id.ic_home){
+            Toast.makeText(this, "Home is clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- 04_2/app/src/main/java/kislyakov/a04_2/NumbersAdapter.java
+++ 04_2/app/src/main/java/kislyakov/a04_2/NumbersAdapter.java
@@ -1,43 +1,143 @@
 package kislyakov.a04_2;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class NumbersAdapter extends RecyclerView.Adapter<NumbersAdapter.NumberViewHolder> {
+public class NumbersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private int itemsCount;
+    public String[] headers;
+    public String[] headersB;
+    public String[] descriptions;
+    public Drawable[] avatars;
+    public Drawable[] avatarsB;
+    public Context parent;
 
-    public NumbersAdapter(int numberOfItems) {
-        itemsCount = numberOfItems;
+
+
+    static final int DESCRIPTION_RED = 1;
+    static final int ONEINTWO = 2;
+    static final int BASECELL = 3;
+    static final int DEFAULT = 0;
+
+    public interface OnItemClickListener {
+        void onItemClick(String item, View view);
+    }
+
+    private final OnItemClickListener listener;
+
+
+    public NumbersAdapter(int items, OnItemClickListener listener) {
+        itemsCount = items;
+        this.listener = listener;
+
     }
 
     @NonNull
     @Override
-    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
         Context context = viewGroup.getContext();
-        int layoutIdForListItem = R.layout.item_tile;
 
-        LayoutInflater inflater = LayoutInflater.from(context);
+        switch (viewType) {
+            case BASECELL: {
+                int layoutIdForListItem = R.layout.base_info_item;
 
-        View view = inflater.inflate(layoutIdForListItem, viewGroup);
 
-        NumberViewHolder viewHolder = new NumberViewHolder(view);
-        viewHolder.descListTextView.setText("LOL");
-        viewHolder.headerListTextView.setText("LIL");
-        viewHolder.imageListItemView.setImageResource(R.drawable.ic_homephone);
-        return viewHolder;
+                LayoutInflater inflater = LayoutInflater.from(context);
+
+                View view = inflater.inflate(layoutIdForListItem, null);
+                NumberViewHolderBase viewHolder = new NumberViewHolderBase(view);
+                Resources resources = context.getResources();
+                headersB = resources.getStringArray(R.array.headers_base);
+
+                TypedArray a = resources.obtainTypedArray(R.array.avatars_base);
+                avatarsB = new Drawable[a.length()];
+                for (int j = 0; j < avatarsB.length; j++) {
+                    avatarsB[j] = a.getDrawable(j);
+                }
+                a.recycle();
+                return viewHolder;
+            }
+            default:{
+                int layoutIdForListItem = R.layout.detail_info_item;
+
+                LayoutInflater inflater = LayoutInflater.from(context);
+
+                View view = inflater.inflate(layoutIdForListItem, null);
+
+                NumberViewHolder viewHolder = new NumberViewHolder(view);
+                Resources resources = context.getResources();
+                headers = resources.getStringArray(R.array.headers);
+                descriptions = resources.getStringArray(R.array.descriptions);
+
+                TypedArray a = resources.obtainTypedArray(R.array.avatars);
+                avatars = new Drawable[a.length()];
+                for (int j = 0; j < avatars.length; j++) {
+                    avatars[j] = a.getDrawable(j);
+                }
+                a.recycle();
+                return viewHolder;
+            }
+
+        }
+
+        //viewHolder.descListTextView.setText("LOL");
+        // viewHolder.headerListTextView.setText("LIL");
+        //viewHolder.imageListItemView.setImageResource(R.drawable.ic_homephone);
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NumberViewHolder numberViewHolder, int i) {
-        numberViewHolder.bind(R.drawable.ic_homephone, "LOL","LIL");
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+            int type = NumbersAdapter.this.getItemViewType(i);
+
+
+            switch (type) {
+                case DESCRIPTION_RED:
+                    NumberViewHolder viewHolderDetail = (NumberViewHolder)viewHolder;
+                    viewHolderDetail.bindRed(avatars[i], headers[i], descriptions[i], listener);
+                    break;
+                case ONEINTWO:
+                    NumberViewHolder viewHolderDetail2 = (NumberViewHolder)viewHolder;
+                    viewHolderDetail2.bindOneInTwo(avatars[i], headers[i], descriptions[i], listener);
+                    break;
+                case BASECELL:
+                    NumberViewHolderBase viewHolderBase = (NumberViewHolderBase)viewHolder;
+                    viewHolderBase.bind(avatarsB[i-7], headersB[i-7], listener);
+                    break;
+                default:
+                    NumberViewHolder viewHolderDetail3 = (NumberViewHolder)viewHolder;
+                    viewHolderDetail3.bind(avatars[i], headers[i], descriptions[i], listener);
+                    break;
+
+
+
+        }
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        // условие для определения айтем какого типа выводить в конкретной позиции
+        if (position == 0 || position == 1) return DESCRIPTION_RED;
+        if (position == 6) return ONEINTWO;
+        if (position >= 7) return BASECELL;
+        return DEFAULT;
     }
 
     @Override
@@ -52,17 +152,85 @@ public class NumbersAdapter extends RecyclerView.Adapter<NumbersAdapter.NumberVi
         TextView descListTextView;
 
         public NumberViewHolder(@NonNull View itemView) {
-            super(itemView);
 
+            super(itemView);
             imageListItemView = itemView.findViewById(R.id.header_pic);
             headerListTextView = itemView.findViewById(R.id.header_tv);
             descListTextView = itemView.findViewById(R.id.header_desc_tv);
         }
 
-        void bind(int imageResourse, String header, String description) {
-            imageListItemView.setImageResource(imageResourse);
+        void bind(Drawable imageResourse, final String header, String description, final OnItemClickListener listener) {
+            imageListItemView.setImageDrawable(imageResourse);
             headerListTextView.setText(header);
             descListTextView.setText(description);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(header, v);
+                }
+            });
+        }
+
+        void bindRed(Drawable imageResourse, final String header, String description, final OnItemClickListener listener) {
+            imageListItemView.setImageDrawable(imageResourse);
+            headerListTextView.setText(header);
+            descListTextView.setTextColor(Color.parseColor("#FF0000"));
+            descListTextView.setText(description);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(header, v);
+                }
+            });
+        }
+
+        void bindOneInTwo(Drawable imageResourse, final String header, String description, final OnItemClickListener listener) {
+            imageListItemView.setImageDrawable(imageResourse);
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) headerListTextView.getLayoutParams();
+            params.removeRule(RelativeLayout.BELOW);
+            //params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            params.addRule(RelativeLayout.RIGHT_OF, R.id.header_pic);
+            headerListTextView.setLayoutParams(params); //causes layout update
+
+            params = (RelativeLayout.LayoutParams) descListTextView.getLayoutParams();
+            params.addRule(RelativeLayout.RIGHT_OF, R.id.header_pic);
+            //params.addRule(RelativeLayout.BELOW, R.id.header_tv);
+            descListTextView.setLayoutParams(params);
+
+            headerListTextView.setText(header);
+            descListTextView.setText(description);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(header, v);
+                }
+            });
+        }
+    }
+
+    class NumberViewHolderBase extends RecyclerView.ViewHolder {
+
+        ImageView imageListItemView;
+        TextView headerListTextView;
+
+        public NumberViewHolderBase(@NonNull View itemView) {
+
+            super(itemView);
+
+            imageListItemView = itemView.findViewById(R.id.header_pic_base);
+            headerListTextView = itemView.findViewById(R.id.header_tv_base);
+
+        }
+
+
+        void bind(Drawable imageResourse, final String header, final OnItemClickListener listener) {
+            imageListItemView.setImageDrawable(imageResourse);
+            headerListTextView.setText(header);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(header, v);
+                }
+            });
+
+
         }
     }
 }
